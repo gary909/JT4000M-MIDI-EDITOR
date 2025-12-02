fbc1113ee88f76177c134853c9dec2cafa2da93b
gary909 <25279957+gary909@users.noreply.github.com>
2025-12-02 15:04:31 +0100

    midi connection now working in browser

--- app.js
+++ app.js
@@ -3,16 +3,23 @@ if (navigator.requestMIDIAccess){
     navigator.requestMIDIAccess().then(onMIDISuccess, onMIDIFailure);
 }
 
-// if midi access:
-function onMIDISuccess(midiAccess){
-    console.log("MIDI ready!");
-    var inputs = midiAccess.inputs; 
-}
-
 // if no midi access:
 function onMIDIFailure(){
     console.log("Could not access your MIDI devices.");
 }
+
+// if midi access:
+function onMIDISuccess(midiAccess){
+    console.log(midiAccess);
+    midiAccess.addEventListener('statechange', updateDevices);
+}
+
+function updateDevices(event){
+    console.log(event);
+    console.log(`Name: ${event.port.name}, Brand: ${event.port.manufacturer}, State: ${event.port.state}, Type: ${event.port.type}`);
+}
+
+
 
 
 
